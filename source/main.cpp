cd784139c4f95c770c77d40dfb466be81be9973a
vrodedanya <socurahlem@yandex.ru>
2020-12-08 12:46:18 +0300

    Added opportunity to execute user script
    New format:
    ./engine <script_name>

--- source/main.cpp
+++ source/main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
 #include "../include/engine.hpp"
 
-int main()
+int main(int argc, char** argv)
 {
-	Simulator sim("../scripts/script.lua");
+	if (argc != 2)
+	{
+		std::cerr << "Format:\n./engine <script_name>" << std::endl;
+		return 1;
+	}
+	Simulator sim(argv[1]);
 
 	sim.loop();
 	return 0;
