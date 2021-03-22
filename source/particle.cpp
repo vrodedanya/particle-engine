a93f0a17bbb232426c074558a08c9622210552b0
vrodedanya <socurahlem@yandex.ru>
2021-03-22 18:14:45 +0300

    Changed functions for draw points

--- source/particle.cpp
+++ source/particle.cpp
@@ -1,7 +1,8 @@
 #include "particle.hpp"
+#include "drawing.hpp"
 
 void Particle::draw(SDL_Renderer* renderer)
 {
-	SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, color.a);
-	SDL_RenderDrawPoint(renderer, position.x(), position.y());
+	tbo::drawing::changeColor(renderer, color.r, color.g, color.b, color.a);
+	tbo::drawing::point(renderer, position.x(), position.y());
 }
